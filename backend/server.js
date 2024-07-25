ed6f73429c59cfc8493541f99641e5bfa870197b
Saurabhn16 <nagpuresaurabh1610@gmail.com>
2024-07-25 08:46:10 +0530

    Your commit message

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,39 @@
+import path from 'path';
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import connectToMongoDB from './db/connectToMongoDB.js';
-import attendanceRoutes from './routes/attendance.routes.js';
 import studentRoutes from './routes/student.routes.js';
+import attendanceRoutes from './routes/attendance.routes.js';
 import scannerRoutes from './routes/scanner.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use('/api/attendance', attendanceRoutes);
+// Middleware
+app.use(bodyParser.json()); // to parse incoming requests with JSON payloads
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
 app.use('/api/students', studentRoutes);
+app.use('/api/attendance', attendanceRoutes);
 app.use('/api/scanners', scannerRoutes);
 
-connectToMongoDB(); 
+// Serve static files from the frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+// Catch-all route to handle client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
+// Start server and connect to MongoDB
 app.listen(port, () => {
+  connectToMongoDB();
   console.log(`Server running on port ${port}`);
 });
